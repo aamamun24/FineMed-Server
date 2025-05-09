1d70b4216c21d4cdc7a686f458d23286bd39e473
Abdullah Al Mamun <bdmamun100@gmail.com>
2025-05-09 22:39:15 +0600

    error and bug fixed

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -2,23 +2,23 @@ import config from "../../../config";
 import catchAsync from "../../../utils/catchAsync";
 import sendResponse from "../../../utils/sendResponse";
 import { authServices } from "./auth.service";
-import httpStatus from 'http-status';
+import httpStatus from "http-status";
 
 const loginUser = catchAsync(async (req, res) => {
   const result = await authServices.loginUserIntoDB(req.body);
   const { refreshToken, accessToken } = result;
 
-  res.cookie('refreshToken', refreshToken, {
+  res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: config.NODE_ENV === 'production', // False for localhost
-    sameSite: config.NODE_ENV === 'production' ? 'none' : 'lax', // Lax for development
+    secure: config.NODE_ENV === "production", // False for localhost
+    sameSite: config.NODE_ENV === "production" ? "none" : "lax", // Lax for development
     maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
   });
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'User is logged in successfully!',
+    message: "User is logged in successfully!",
     data: {
       accessToken,
     },
@@ -29,15 +29,10 @@ const refreshToken = catchAsync(async (req, res) => {
   const { refreshToken } = req.cookies;
   const result = await authServices.refreshToken(refreshToken);
 
-
-  // console.log("refreshed token")
-
-
-
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'Access token is retrieved successfully!',
+    message: "Access token is retrieved successfully!",
     data: result,
   });
 });
